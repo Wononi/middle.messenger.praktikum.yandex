70db4437f9b80378318ab4e71c110495782e00be
Petr Bychkov <wononi02144@gmail.com>
2023-03-15 12:59:13 +0300

    add error pages

---
+++ src/pages/notFound/notFound.js
@@ -0,0 +1,13 @@
+import Handlebars from 'handlebars';
+import './notFound.scss'
+
+const notFound = () => {
+  return Handlebars.compile(`
+    <div class="notFound">
+        Страница не найдена<br>404
+        <a href="/home">Вернуться на главную</a>
+    </div>
+  `)()
+}
+
+export default notFound
